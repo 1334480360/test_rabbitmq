9a00d514d97633bd921b2bdafed18dc720725f2d
chenbingxuan <chenbingxuan@dafycredit.com>
2018-10-11 18:00:29 +0800

    查询集群节点信息

--- src/main/java/com/giveu/test/enums/WebApi.java
+++ src/main/java/com/giveu/test/enums/WebApi.java
@@ -6,16 +6,27 @@ package com.giveu.test.enums;
  * @date：2018/10/10
  */
 public enum WebApi {
-	EXCHANGE_LIST(1, "exchanges"),
-	USER_LIST(1, "users"),
+	EXCHANGE_LIST(1, "exchanges", "交换器列表"),
+	USER_LIST(2, "users", ""),
+	NODE_LIST(3, "nodes", ""),
 	;
 
 	private int code;
 	private String value;
+	private String desc;
 
-	WebApi(int code, String value) {
+	WebApi(int code, String value, String desc) {
 		this.code = code;
 		this.value = value;
+		this.desc = desc;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
 	}
 
 	public int getCode() {
